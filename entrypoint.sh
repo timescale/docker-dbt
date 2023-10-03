7fd3e54ff2917c012ee6dbcb759181df65ec0ca7
Matthew Peveler <matt@popsql.com>
2023-10-03 10:13:44 -0400

    if using env, do not look at mounts

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,19 @@
 
 if [ -n "${DBT_PROFILES}" ]; then
   echo "${DBT_PROFILES}" > /.dbt/profiles.yml
-elif [ -f ~/.dbt/profiles.yml ]; then
-  cp ~/.dbt/profiles.yml /.dbt/profiles.yml
-fi
-
-if [ -n "${AWS_CREDENTIALS}" ]; then
-  echo "${AWS_CREDENTIALS}" > /.dbt/aws_credentials
-elif [ -f ~/.aws/credentials ]; then
-  cp ~/.aws/credentials /.dbt/aws_credentials
-fi
-
-if [ -n "${BQ_KEYFILE}" ]; then
-  echo "${BQ_KEYFILE}" > /.dbt/bq_keyfile.json
+  if [ -n "${AWS_CREDENTIALS}" ]; then
+    echo "${AWS_CREDENTIALS}" > /.dbt/aws_credentials
+  fi
+  if [ -n "${BQ_KEYFILE}" ]; then
+    echo "${BQ_KEYFILE}" > /.dbt/bq_keyfile.json
+  fi
+else
+  if [ -f ~/.dbt/profiles.yml ]; then
+    cp ~/.dbt/profiles.yml /.dbt/profiles.yml
+  fi
+  if [ -f ~/.aws/credentials ]; then
+    cp ~/.aws/credentials /.dbt/aws_credentials
+  fi
 fi
 
 exec "$@"
